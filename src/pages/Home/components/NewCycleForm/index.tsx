0afd0e082c571bf9926184c20003728f47119194
Ruan Cardoso dos Santos <cardosoruan2001@gmail.com>
2024-02-28 12:01:16 -0300

    fix: New Cycle timer

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -31,7 +31,7 @@ export function NewCycleForm() {
         type="number"
         placeholder="00"
         step={5}
-        min={1}
+        // min={1}
         max={60}
         disabled={!!activeCycle}
         {...register('minutesAmount', { valueAsNumber: true })}
